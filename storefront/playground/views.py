2332a1e70b9ef37da4e307a06c15ad46e4843adb
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-12-29 10:45:39 +0600

    Debugger problem, need to solve

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+# request ->  response
+# request handler
+# action
+
+def say_hello(request):
+    # pull data from db
+    # transform data
+    # send emails
+    x = 1
+    y = 2
+    return render(request, 'hello.html', {'name': 'Rakin'})
